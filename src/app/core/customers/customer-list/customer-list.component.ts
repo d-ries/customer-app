9a6fbd8876b58b114c17422b1a3fee06a815aa49
d-ries <swinnendries@gmail.com>
2023-11-27 15:00:46 +0100

    cleanup

--- src/app/core/customers/customer-list/customer-list.component.ts
+++ src/app/core/customers/customer-list/customer-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { AsyncPipe, CommonModule, NgFor } from '@angular/common';
+import { AsyncPipe, NgFor } from '@angular/common';
 import { FilterComponent } from '../filter/filter.component';
 import { CustomerItemComponent } from '../customer-item/customer-item.component';
 import { Filter } from '../../../shared/models/filter.model';
@@ -24,15 +24,17 @@ export class CustomerListComponent {
   handleFilter(filter: Filter){
     this.customerList$ = this.customerList$.pipe(
       map((data: Customer[]) => {
-          return data.filter(customer=>{
-            const matchesName = customer.name.toLowerCase().includes(filter.name.toLowerCase());
-            const matchesCity = customer.city.toLowerCase().includes(filter.city.toLowerCase());
-            const matchesVat = filter.vat ? customer.vat == filter.vat : true;
-
-            return matchesName && matchesCity && matchesVat;
-          })
+          return data.filter(customer=>this.isCustomerMatchingFilter(customer, filter))
       })
     )
-}
+  }
+
+  private isCustomerMatchingFilter(customer: Customer, filter: Filter): boolean {
+    const matchesName = customer.name.toLowerCase().includes(filter.name.toLowerCase());
+    const matchesCity = customer.city.toLowerCase().includes(filter.city.toLowerCase());
+    const matchesVat = filter.vat ? customer.vat === filter.vat : true;
+
+    return matchesName && matchesCity && matchesVat;
+  }
 
 }
